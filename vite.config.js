caa2f135b08948f202cb0b7b0ab119c17c18450a
sekareuejddiidwei <97535807+sekareuejddiidwei@users.noreply.github.com>
2026-06-29 06:29:23 +0700

    lin

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import laravel from 'laravel-vite-plugin';
+import { bunny } from 'laravel-vite-plugin/fonts';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/scss/app.scss', 'resources/js/app.js'],
+            refresh: true,
+            fonts: [
+                bunny('Instrument Sans', {
+                    weights: [400, 500, 600],
+                }),
+            ],
+        }),
+    ],
+    server: {
+        watch: {
+            ignored: ['**/storage/framework/views/**'],
+        },
+    },
+});
